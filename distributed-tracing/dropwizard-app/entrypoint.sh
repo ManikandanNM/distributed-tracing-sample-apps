207c6ddbfd3333103b8eb36e51520e06957d7516
ManikandanNM <33888088+ManikandanNM@users.noreply.github.com>
2020-06-27 18:03:37 +0530

    Update entrypoint.sh

--- distributed-tracing/dropwizard-app/entrypoint.sh
+++ distributed-tracing/dropwizard-app/entrypoint.sh
@@ -3,8 +3,7 @@ enableProxy=$1
 proxyIPOrDIToken=$2
 applicationName=$3
 flushInterval=$4
-echo "$enableProxy" >> ./sample.text
-echo "$proxyIPOrDIToken" >> ./sample.text
-echo "$applicationName" >> ./sample.text
-echo "$flushInterval" >> ./sample.text
-sleep 300
+java -jar ./shopping/target/shopping-1.0-SNAPSHOT.jar "$enableProxy" "$proxyIPOrDIToken" "$applicationName" server ./shopping/app.yaml &
+java -jar ./styling/target/styling-1.0-SNAPSHOT.jar "$enableProxy" "$proxyIPOrDIToken" "$applicationName" server ./styling/app.yaml &
+java -jar ./delivery/target/delivery-1.0-SNAPSHOT.jar "$enableProxy" "$proxyIPOrDIToken" "$applicationName" server ./delivery/app.yaml &
+./loadgen.sh "$flushInterval"
