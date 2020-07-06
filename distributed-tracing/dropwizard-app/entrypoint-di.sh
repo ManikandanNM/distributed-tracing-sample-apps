f1cd9dad26ebf725644588b3d86d24d8974516f4
ManikandanNM <33888088+ManikandanNM@users.noreply.github.com>
2020-07-06 17:03:25 +0530

    Update entrypoint-di.sh

--- distributed-tracing/dropwizard-app/entrypoint-di.sh
+++ distributed-tracing/dropwizard-app/entrypoint-di.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 enableProxy=$1
-DICluster=
-DIToken=$2
-batchSize=$3
-queueSize=$4
-DIflushInteval=$5
-applicationName=$6
-loadgenFlushInterval=$7
+DICluster=$2
+DIToken=$3
+batchSize=$4
+queueSize=$5
+DIflushInteval=$6
+applicationName=$7
+loadgenFlushInterval=$8
 java -jar ./shopping/target/shopping-1.0-SNAPSHOT.jar "$enableProxy" "$DICluster" "$DIToken" "$batchSize" "$queueSize" "$DIflushInteval" "$applicationName" server ./shopping/app.yaml &
 java -jar ./styling/target/styling-1.0-SNAPSHOT.jar "$enableProxy" "$DICluster" "$DIToken" "$batchSize" "$queueSize" "$DIflushInteval" "$applicationName" server ./styling/app.yaml &
 java -jar ./delivery/target/delivery-1.0-SNAPSHOT.jar "$enableProxy" "$DICluster" $DIToken "$batchSize" "$queueSize" "$DIflushInteval" "$applicationName" server ./delivery/app.yaml &
