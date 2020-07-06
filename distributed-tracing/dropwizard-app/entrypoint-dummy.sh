e904007f4708a21ad2af40f22b08e912d935f1b6
ManikandanNM <33888088+ManikandanNM@users.noreply.github.com>
2020-07-06 14:23:45 +0530

    Create entrypoint-dummy.sh

---
+++ distributed-tracing/dropwizard-app/entrypoint-dummy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+enableProxy=$1
+DICluster=
+DIToken=$2
+batchSize=$3
+queueSize=$4
+DIflushInteval=$5
+applicationName=$6
+loadgenFlushInterval=$7
+echo "$enableProxy" 
+echo "$DICluster"
+echo "$DIToken"
+echo "$batchSize"
+echo "$queueSize"
+echo "$DIflushInteval"
+echo "$applicationName"
+echo "$loadgenFlushInterval"
